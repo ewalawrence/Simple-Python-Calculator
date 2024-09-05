8a8875b329734c183c0588edef0b2270e871445b
Lawrence Ewa <ewalawrence1991@gmail.com>
2024-09-05 10:09:13 +0100

    + - * / computation

---
+++ simpleCalculator.py
@@ -0,0 +1,20 @@
+# Python Simple Calculator
+
+operator = input("Enter an Operator(+ - * /): ")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print(round(result, 3))
+elif operator == "-":
+    result = num1 - num2
+    print(round(result, 3))
+elif operator == "*":
+    result = num1 * num2
+    print(round(result, 3))
+elif operator == "/":
+    result = num1 / num2
+    print(round(result, 3))
+else:
+    print(f"{operator} is an invalid operator")
